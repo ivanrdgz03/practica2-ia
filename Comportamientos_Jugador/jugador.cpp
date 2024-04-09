3320e684e7e885f98def6b4667de0cea6263755e
ivan <ivanfrisonivan@gmail.com>
2024-04-09 22:20:19 +0200

    Actualizar ComportamientoJugador para implementar el nivel 0 y actualizar CMakeList.txt

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -6,18 +6,103 @@
 #include <set>
 #include <stack>
 
-// Este es el método principal que se piden en la practica.
-// Tiene como entrada la información de los sensores y devuelve la acción a realizar.
-// Para ver los distintos sensores mirar fichero "comportamiento.hpp"
-Action ComportamientoJugador::think(Sensores sensores)
+stateN0 ComportamientoJugador::applyAction(const stateN0 &state, const Action &accion) const
+{
+	stateN0 newState = state;
+
+
+
+	switch (accion)
+	{
+	case actWALK:
+		switch (newState.jugador.brujula)
+		{
+		case 0:
+			newState.jugador.f--;
+			break;
+		case 1:
+			newState.jugador.c++;
+			break;
+		case 2:
+			newState.jugador.f++;
+			break;
+		case 3:
+			newState.jugador.c--;
+			break;
+		}
+		break;
+	case actTURN_SR:
+		newState.jugador.brujula = (Orientacion)((newState.jugador.brujula + 1) % 8);
+		break;
+	case actTURN_L:
+		newState.jugador.brujula = (Orientacion)((newState.jugador.brujula + 6) % 8);
+		break;
+	case actIDLE:
+		break;
+	default:
+		throw("Acción no reconocida");
+	}
+
+	return newState;
+}
+
+bool ComportamientoJugador::busquedaN0(const Sensores &sensores)
+{
+	
+}
+
+Action ComportamientoJugador::nivel0(const Sensores &sensores)
 {
 	Action accion = actIDLE;
 
-	// Incluir aquí el comportamiento del agente jugador
+	current_state.st.jugador.f = sensores.posF;
+	current_state.st.jugador.c = sensores.posC;
+	current_state.st.jugador.brujula = sensores.sentido;
+
+	current_state.st.colaborador.f = sensores.CLBposF;
+	current_state.st.colaborador.c = sensores.CLBposC;
+	current_state.st.colaborador.brujula = sensores.CLBsentido;
+
+	if (hayPlan && !plan.empty())
+	{
+		accion = plan.front();
+		plan.pop_front();
+	}
+	else if (busquedaN0(sensores))
+	{
+		accion = plan.front();
+		plan.pop_front();
+	}
+
+	if (plan.empty())
+		hayPlan = false;
 
 	return accion;
 }
 
+Action ComportamientoJugador::think(Sensores sensores)
+{
+	Action accion = actIDLE;
+
+	if (sensores.nivel != 4)
+		switch (sensores.nivel)
+		{
+		case 0:
+			accion = nivel0(sensores);
+			break;
+		case 1:
+		case 2:
+		case 3:
+		default:
+			throw("Nivel no implementado");
+			break;
+		}
+	else
+	{
+		throw("Nivel no implementado");
+	}
+	return accion;
+}
 
 int ComportamientoJugador::interact(Action accion, int valor)
 {
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -5,6 +5,28 @@
 
 #include <list>
 
+struct stateN0{
+  ubicacion jugador;
+  ubicacion colaborador;
+  Action ultimaAccionColaborador;
+
+  bool operator==(const stateN0& other) const{
+    return jugador == other.jugador && colaborador == other.colaborador;
+  }
+};
+
+struct nodeN0{
+  stateN0 st;
+  list<Action> secuencia;
+
+  bool operator==(const nodeN0& other) const{
+    return st == other.st;
+  }
+
+  bool operator<(const nodeN0& other) const{
+    return ((st.jugador.f < other.st.jugador.f)||(st.jugador.f == other.st.jugador.f && st.jugador.c < other.st.jugador.c));
+  }
+};
 
 class ComportamientoJugador : public Comportamiento {
   public:
@@ -12,7 +34,14 @@ class ComportamientoJugador : public Comportamiento {
       // Inicializar Variables de Estado
     }
     ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
-      // Inicializar Variables de Estado
+      hayPlan = false;
+
+      current_state.st.jugador = {0,0};
+      current_state.st.colaborador = {0,0};
+      current_state.st.ultimaAccionColaborador = actIDLE;
+      current_state.secuencia.clear();
+
+
     }
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
     ~ComportamientoJugador(){}
@@ -22,7 +51,13 @@ class ComportamientoJugador : public Comportamiento {
 
 
   private:
-    // Declarar Variables de Estado
+    bool hayPlan;
+    list<Action> plan;
+    nodeN0 current_state;
+
+    stateN0 applyAction(const stateN0& state, const Action& accion) const;
+    bool busquedaN0(const Sensores& sensores);
+    Action nivel0(const Sensores& sensores);
 
 
 
